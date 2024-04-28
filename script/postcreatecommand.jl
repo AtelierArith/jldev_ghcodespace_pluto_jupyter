557d0fc049f6d1e8a85b796eef1de2f7021e6bd6
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2024-04-28 21:40:18 +0900

    update

--- script/postcreatecommand.jl
+++ script/postcreatecommand.jl
@@ -1,6 +1,9 @@
 using Pkg
+
 Pkg.activate()
+# Install development packages into the default shared environment.
 Pkg.add(["Revise", "BenchmarkTools", "JuliaFormatter", "IJulia", "Pluto", "PlutoUI"])
+Pkg.precompile()
 
 using Pkg
 Pkg.activate(pwd())
